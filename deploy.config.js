c80468864a3fb9bdfd7f41b0a34d6ebd3f1b3c96
niuyueyang <l>
2020-10-22 11:51:15 +0800

    脚手架模板

---
+++ deploy.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    projectName: '', // 项目名称
+    // 根据需要进行配置，如只需部署prod线上环境，请删除dev测试环境配置，反之亦然，支持多环境部署
+    dev: { // 测试环境
+        name: '测试环境',
+        script: "npm run build", // 测试环境打包脚本
+        host: '', // 测试服务器地址
+        port: 21, // ssh port，一般默认22
+        user: '', // 登录服务器用户名
+        password: '', // 登录服务器密码
+        distPath: 'dist',  // 本地打包dist目录
+        webDir: '/t1/star-pc-upload/web1',  // // 测试环境服务器地址
+    },
+    prod: {  // 线上环境
+        name: '线上环境',
+        script: "npm run build", // 线上环境打包脚本
+        host: '', // 线上服务器地址
+        port: 21, // ssh port，一般默认22
+        user: '', // 登录服务器用户名
+        password: '', // 登录服务器密码
+        distPath: 'dist',  // 本地打包dist目录
+        webDir: '' // 线上环境web目录
+    }
+}
