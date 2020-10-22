f43dac138d4d4590e65c498d8ce38b7117c28916
niuyueyang <3223891963@qq.com>
2020-10-22 14:57:34 +0800

    Update deploy.config.js

--- deploy.config.js
+++ deploy.config.js
@@ -5,17 +5,17 @@ module.exports = {
         name: '测试环境',
         script: "npm run build", // 测试环境打包脚本
         host: '', // 测试服务器地址
-        port: 21, // ssh port，一般默认22
+        port: 21, // ftp port，一般默认21
         user: '', // 登录服务器用户名
         password: '', // 登录服务器密码
-        distPath: 'dist',  // 本地打包dist目录
-        webDir: '/t1/star-pc-upload/web1',  // // 测试环境服务器地址
+        distPath: '',  // 本地打包dist目录
+        webDir: '',  // // 测试环境服务器地址
     },
     prod: {  // 线上环境
         name: '线上环境',
         script: "npm run build", // 线上环境打包脚本
         host: '', // 线上服务器地址
-        port: 21, // ssh port，一般默认22
+        port: 21, // ftp port，一般默认21
         user: '', // 登录服务器用户名
         password: '', // 登录服务器密码
         distPath: 'dist',  // 本地打包dist目录
